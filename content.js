7c056397fd4dad4e5f28ba6be5cbc5b44aa6cd2b
KitasanYuu <tamamoratizel@gmail.com>
2025-02-18 17:15:34 +0800

    fix: Hangar entry missing
    Fixed the issue that the Hangar entry was not added correctly when opening the drawer when the page was not fully loaded

--- content.js
+++ content.js
@@ -37,36 +37,51 @@
       }
     })
 
-    // 启动监听
-    observer.observe(element, { childList: true });
+    // 监听子节点变化，并对子树内的变化也进行监听
+    observer.observe(element, { childList: true, subtree: true });
   }
 
-	/**
-	 * 复制原有按钮，将其修改为机库按钮并插入到原来的按钮的上方
-	 * @param rsiSidePanel 侧边抽屉 dom
-	 */
+  /**
+   * 复制原有按钮，将其修改为机库按钮并插入到原按钮的上方
+   * 在插入前先检查是否已经存在对应的按钮
+   * @param {HTMLElement} button 原始按钮
+   */
   function copyAndAddButton(button) {
 		console.log('bbbbbbbbbbbbbb', button)
-    if (button) {
+    if (!button) return;
+    
+    // 如果已经存在 href 为 /account/pledges 的按钮，则不再添加
+    if (button.parentNode.querySelector('a[data-cy-id="button"][href="/account/pledges"]')) {
+      return;
+    }
       // 复制元素
       const hangarButton = button.cloneNode(true);
 
       // 修改 href
       hangarButton.href = "/account/pledges";
 
-			// 查找按钮的文本部分的 dom
-			const hangarButtonText = hangarButton.querySelector('span[data-cy-id="button__text"]');
+			// 查找按钮中的文本部分
+      const hangarButtonText = hangarButton.querySelector('span[data-cy-id="button__text"]');
+      if (hangarButtonText) {
+        hangarButtonText.innerText = "Your Hanger";
+      }
 
 			// 修改文本
 			hangarButtonText.innerText = "Your Hanger"
 
       // 插入到目标元素的前方
       button.parentNode.insertBefore(hangarButton, button);
-    }
   }
 
   // 开始查找抽屉，如果找到执行监听回调
   waitForElement(document, "#sidePanel", (risSidePanel) => {
     startObserve(risSidePanel)
+
+    // 初始检查：防止首次打开抽屉时按钮已经存在，MutationObserver 不触发
+    const button = sidePanel.querySelector('a[data-cy-id="button"][href="/account/settings"]');
+    if (button) {
+      copyAndAddButton(button);
+    }
+
   });
 })();
